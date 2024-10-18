6649f4487e10b97e1eae7190adc27edcc277243e
ryuuzera <faag_@live.com>
2024-10-17 23:17:10 -0300

    first commit

---
+++ Workshop.WebScraping.Practice/ClientHttp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.WebScraping.Practice
+{
+    internal class ClientHttp : IDisposable
+    {
+        private readonly HttpClient httpClient;
+
+        private readonly CookieContainer cookieContainer;
+
+        private static ClientHttp currentInstance;
+        public ClientHttp()
+        {
+            if (currentInstance == null)
+            {
+                cookieContainer = new CookieContainer();
+
+                var handler = new HttpClientHandler
+                {
+                    CookieContainer = cookieContainer
+                };
+
+                httpClient = new HttpClient(handler)
+                {
+                    Timeout = TimeSpan.FromSeconds(15)
+                };
+            }
+        }
+
+        public static HttpClient New()
+        {
+            if (currentInstance == null)
+            {
+                currentInstance = new ClientHttp();
+            }
+
+            return currentInstance.httpClient;
+        }
+        public void Dispose()
+        {
+            httpClient.Dispose();
+        }
+    }
+}
---
+++ Workshop.WebScraping.Practice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Workshop.WebScraping.Practice;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var httpClient = ClientHttp.New();
+
+        Console.WriteLine("Workshop - Webscraping Aula 2");
+    }
+}
